09831c7183542c9f1c86eafb785066e83a7f067e
Charles Stephan K <143457657+charghb@users.noreply.github.com>
2024-05-23 03:42:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
         { name: 'card4', img: 'images/rollsafe.png' },
         { name: 'card5', img: 'images/success.png' },
         { name: 'card5', img: 'images/success.png' },
+        { name: 'card6', img: 'images/Tableau 1.png' },
+        { name: 'card6', img: 'images/Tableau 1.png' },
         // ...add more pairs as needed
     ];
 
