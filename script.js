1076d25d4299a76b6e33ca2a285d705cc9b9b573
Charles Stephan K <143457657+charghb@users.noreply.github.com>
2024-05-23 12:44:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = 0;
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
+        { name: 'card1', img: 'images/steph.png.jpg' },
+        { name: 'card1', img: 'images/steph.png.jpg' },
         { name: 'card2', img: 'images/drake.png' },
         { name: 'card2', img: 'images/drake.png' },
         { name: 'card3', img: 'images/fine.png' },
